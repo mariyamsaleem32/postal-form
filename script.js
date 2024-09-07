5152a3f2b7f5ba202c9ad239777ee4e2641418aa
mariyamsaleem32 <mariyamsaleem87@gmail.com>
2024-09-08 00:25:56 +0500

    complete the form

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkAddress() {
+    let val = document.getElementById("postal").value;
+   
+   switch (val) {
+       case "05444":
+           alert("it's karachi's postal code")
+           break;
+       case "05444":
+           alert("it's karachi's postal code")
+           break;
+      case "09454":
+           alert("it's faislabaad's postal code")
+           break;
+       
+       default:
+           alert("it's not valid postal code")
+           break;
+   }
+    };
+   
